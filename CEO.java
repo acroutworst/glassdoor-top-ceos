6fafb32836aff32930b3eac981db4c98a7153c3d
adam <croutworsta@gmail.com>
2018-11-04 21:38:32 -0800

    support CEO object

---
+++ CEO.java
@@ -0,0 +1,55 @@
+public class CEO {
+    int rating;
+    String name;
+    String face;
+    String companyLogo;
+    String companyName;
+
+    public CEO() {
+        rating = 100;
+        name = "";
+        face = "";
+        companyLogo = "";
+        companyName = "";
+    }
+
+    public String getRating() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFace() {
+        return name;
+    }
+
+    public String getLogo() {
+        return name;
+    }
+
+    public String getCompany() {
+        return name;
+    }
+
+    public void setName(String param) {
+        name = param;
+    }
+
+    public void setRating(int param) {
+        rating = param;
+    }
+
+    public void setFace(String param) {
+        face = param;
+    }
+
+    public void setLogo(String param) {
+        companyLogo = param;
+    }
+
+    public void setCompany(String param) {
+        companyName = param;
+    }
+}
